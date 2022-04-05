8b72ac165eb5f68d6801780955f13ce861e7e2d7
Lucas <lucas.michel@student.uliege.be>
2022-04-05 18:55:26 +0200

    adding unormalization

--- utils/images.py
+++ utils/images.py
@@ -10,8 +10,15 @@ def tensor_to_pil(labs:torch.Tensor):
     """
        Transform tensors using lab colorspace to a RGB PIL image
     """
+    
     images = []
     for lab in labs:
+        
+        lab[0] = (lab[0] + 1)*50
+        for i in [1,2]:
+            lab[i] = lab[i]*128
+        
+
         pil_lab = np.array(lab.permute(1, 2, 0))
         arr_lab = np.around((color.lab2rgb(pil_lab) * 255)).astype("uint8")
         images.append(Image.fromarray(arr_lab))
