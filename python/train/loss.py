6f63e3ddc837a889dfd77d21f65811efdb36ea89
Julien-Gustin <juliengustin02@gmail.com>
2022-04-08 16:06:00 +0200

    Train generator done

--- python/train/loss.py
+++ python/train/loss.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 from torch.nn.modules.loss import _Loss
-class cGANValue(_Loss): #jsp pq les loss héritent de Module : https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html#L1Loss
+
+
+
+class cGANLoss(_Loss): #jsp pq les loss héritent de Module : https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html#L1Loss
     ## a la respondabilité de gérer les labels des true/fake prediction et de calculer la loss de prédiction labelisée
     def __init__(self, real_label=1.0, fake_label=0.0):
         super().__init__()
@@ -27,9 +30,11 @@ class cGANValue(_Loss): #jsp pq les loss héritent de Module : https://pytorch.o
 
     For discrimantor:
         loss = cGANValue(fake, False)
+
+    Solution: https://arxiv.org/pdf/1406.2661.pdf 
     """
     def __call__(self, preds, target_is_real):
         labels = self.real_label if target_is_real else self.fake_label
         labels = labels.expand_as(preds) # on en fait un tensor de la même taille que preds full de 1 ou 0
-        return -self.loss(preds, labels) # attention au - ici
+        return self.loss(preds, labels) # attention au - ici
 
--- python/train/trainer.py
+++ python/train/trainer.py
@@ -1,7 +1,132 @@
+from matplotlib.pyplot import axis
 import torch
 import torch.nn as nn
 
+from .loss import cGANLoss
+
+# https://arxiv.org/pdf/1406.2661.pdf (train GAN)
+class GanTrain():   
+    def __init__(self, generator:nn.Module, discriminator:nn.Module) -> None:
+        self.generator = generator
+        self.discriminator = discriminator
+
+        self.cgan_loss = cGANLoss()
+        self.l1_loss = nn.L1Loss()
+
+        # TODO: hyperparameters in arguments
+        self.optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+        self.optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+        self.gamma = 0.5
+
+    def set_requires_grad(self, model:nn.Module, requires_grad:bool):
+        for param in model.parameters():
+            param.requires_grad = requires_grad 
+
+    def generator_loss(self, L:torch.Tensor, ab_reel:torch.Tensor):
+        """ Compute the loss of the generator according to a weighted sum of L1 loss and GAN loss """
+        ab_fake = self.generator(L)
+        Lab_fake = torch.cat((L, ab_fake), axis=1)
+        discriminator_confidence = self.discriminator(Lab_fake)
+
+        l1_loss = self.l1_loss(ab_fake, ab_reel)
+        gan_loss = self.cgan_loss(discriminator_confidence, True) # trick
+
+        loss = l1_loss * self.gamma + gan_loss
+
+        return loss
+
+    def generator_step(self, L:torch.Tensor, ab_reel:torch.Tensor):
+        """ Perform a one step generator training """
+        loss = self.generator_loss(L, ab_reel)
+        loss.backwards()
+
+        self.optimizer_G.step()
+        self.optimizer_G.zero_grad()
+
+        return loss
+
+
+    def discriminator_backward(self):
+        # Houyon TODO
+        pass
+
+    def step(self, L:nn.Module, ab_reel:nn.Module):
+        # Houyon TODO
+
+        d_loss = self.discriminator_backward()
+
+        # Julien
+        self.set_requires_grad(self.discriminator, False)
+        g_loss = self.generator_step(L, ab_reel)
+        self.set_requires_grad(self.discriminator, True)
+
+        return d_loss.detach(), g_loss.detach()
+
+
+def train(num_epochs, generator:nn.Module, discriminator:nn.Module, trainloader, testloader):
+    gan_train = GanTrain(generator, discriminator)
+
+    train_g_avg_loss = []
+    train_d_avg_loss = []
+    test_g_avg_loss = []
+    test_d_avg_loss = []
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    for i in range(num_epochs):
+        train_g_losses = []
+        train_d_losses = []
+        test_g_losses = []
+        test_d_losses = []
+        
+        for L, ab in trainloader:
+            L = L.to(device)
+            ab = ab.to(device)
+
+            g_loss, d_loss = gan_train.step(L, ab)
+
+            train_g_losses.append(g_loss.detach())
+            train_d_losses.append(d_loss.detach())        
+
+        with torch.no_grad():   
+            for L, ab in testloader:
+                L = L.to(device)
+                ab = ab.to(device)
+
+                g_loss = gan_train.generator_loss(L, ab)
+                d_loss = gan_train.discriminator_loss(L, ab)
+                
+                test_g_losses.append(g_loss.detach())
+                test_d_losses.append(g_loss.detach())
+
+            train_g_avg_loss.append(torch.mean(torch.Tensor(train_g_losses)))
+            train_d_avg_loss.append(torch.mean(torch.Tensor(train_d_losses)))
+            test_g_avg_loss.append(torch.mean(torch.Tensor(test_g_losses)))
+            test_d_avg_loss.append(torch.mean(torch.Tensor(test_d_losses)))
+
+            print('[Epoch {}/{}] '.format(i+1, num_epochs) + "\n\t--- Generator ---\n"
+                  '\ttrain_loss: {:.4f} - '.format(train_g_avg_loss[-1]) +
+                  'test_loss: {:.4f} - '.format(test_g_avg_loss[-1]) + "\n\t--- Discriminator ---\n"
+                  'train_loss: {:.4f} - '.format(train_d_avg_loss[-1]) +
+                  'test_loss: {:.4f}'.format(test_d_avg_loss[-1]))
+
+    return train_g_avg_loss, train_d_avg_loss, test_g_avg_loss, test_d_avg_loss
+
+
+
+
+
+
+
+
+
+
+
+
 LEARNING_RATE = 0.01
+
+
 
 def train_G_L1(num_epochs, generator, trainloader, testloader):
     criterion = nn.L1Loss()
@@ -55,3 +180,5 @@ def train_G_L1(num_epochs, generator, trainloader, testloader):
                   'test_loss: {:.4f}'.format(test_avg_loss[-1]))
 
     return train_avg_loss, test_avg_loss
+
+
